09378380dc44bd48e98982fde571258cd05e5811
WithoutAName25 <withoutaname25@gmail.com>
2020-12-23 13:50:14 +0100

    Little optimizations

--- src/main/java/withoutaname/mods/withoutaxmas/modules/present/blocks/PresentScreen.java
+++ src/main/java/withoutaname/mods/withoutaxmas/modules/present/blocks/PresentScreen.java
@@ -3,9 +3,12 @@ package withoutaname.mods.withoutaxmas.modules.present.blocks;
 import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.text.ITextComponent;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
 import withoutaname.mods.withoutalib.blocks.BaseScreen;
 import withoutaname.mods.withoutaxmas.WithoutAXmas;
 
+@OnlyIn(Dist.CLIENT)
 public class PresentScreen extends BaseScreen<PresentContainer> {
 
 	public PresentScreen(PresentContainer container, PlayerInventory playerInventory, ITextComponent title) {
